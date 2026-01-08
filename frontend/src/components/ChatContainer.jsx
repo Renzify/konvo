a4a2e70c834b11385cc6a667abb44e10f8a3bf3d
Lorens <168178279+Renzify@users.noreply.github.com>
2026-01-08 11:39:53 +0800

    add ChatHeader component for ChatContainer

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import { useAuthStore } from "../store/useAuthStore";
+import ChatHeader from "./ChatHeader";
 
 function ChatContainer() {
-  return <div></div>;
+  const { selectedUser, getMessagesByUserId, messages } = useChatStore();
+  const { authUser } = useAuthStore();
+
+  useEffect(() => {
+    getMessagesByUserId(selectedUser._id);
+  }, [selectedUser, getMessagesByUserId]);
+
+  return (
+    <>
+      <ChatHeader />
+    </>
+  );
 }
 
 export default ChatContainer;
